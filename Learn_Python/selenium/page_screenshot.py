89a66c24b1272d6ecfc6dfdf66d86bf66d674986
Kevin Guan <KevinGuan.gm@gmail.com>
2015-12-26 18:20:41 +0800

    Fixed typo; Removed useless code

--- Learn_Python/selenium/page_screenshot.py
+++ Learn_Python/selenium/page_screenshot.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from seleium import webdriver
+from selenium import webdriver
 
 driver = webdriver.PhantomJS()
 driver.maximize_window()
@@ -7,7 +7,7 @@ driver.maximize_window()
 driver.get(input('Link: '))
 
 for i in [i/100 for i in range(10, 999)]:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight/{0});"
+    driver.execute_script("scroll(0, document.body.scrollHeight/{0});"
                           .format(i))
 
 driver.save_screenshot(input('File name: '))
