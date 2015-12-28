425c9e3801e90701e69b340d3a18fac0ab8ebbc4
Kevin Guan <KevinGuan.gm@gmail.com>
2015-12-28 11:59:28 +0800

    Just check the document of `dict.get()`
    Why did I write this program? The Python official document of
    `dict.get()` is enough:
    
    https://docs.python.org/3/library/stdtypes.html#dict.get

--- Learn_Python/basic/dict_get.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-test_dict = {'a': 'Key `a` is in this dict.'}
-
-print(test_dict.get('a', "Key `a` isn't in this dict"))
-print(test_dict.get('b', "Key `b` isn't in this dict"))
