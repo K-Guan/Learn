8aa0df1ea3507e518d021cf45be1abff45fa6507
Kevin Guan <KevinGuan.gm@gmail.com>
2015-12-28 11:46:12 +0800

    Added some comments; Rewrite to clarify "things"
    Changed the output to clarify what do I want to tell others. Also added
    some comments.

--- Learn_Python/basic/color.py
+++ Learn_Python/basic/color.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
+"""
+A small script which could print a test string
+in all available color, format of the console (works on NT and Unix)
+
+To let you know how to print text 'prettily'.
+"""
+
+formats = []
+
 for i in range(0, 10):
-    print('\033[{0}m{0}: This is a test\033[{0};m'.format(i))
-
+    formats.append(i)
 for i in range(30, 48):
-    print('\033[{0}m{0}: This is a test\033[{0};m'.format(i))
-
+    formats.append(i)
 for i in range(90, 108):
-    print('\033[{0}m{0}: This is a test\033[{0};m'.format(i))
+    formats.append(i)
+
+print('Demo:'+'\n')
+for i in formats:
+    # `fn` means `format_number` here
+    print('\033[{fn}m   The demo of format "{fn}"   \033[{fn};m'
+          .format(fn=i))
