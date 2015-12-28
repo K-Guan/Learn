61f59e0b7cce6eff7dbf63780d80d27f7b0ebd25
Kevin Guan <KevinGuan.gm@gmail.com>
2015-12-28 13:47:33 +0800

    Removed useless scripts

--- Learn_JavaScript/basic/ask_and_confirm.js
+++
@@ -1,8 +0,0 @@
-var name = prompt('Hey, what is your name?');
-
-while (confirm('So your name is '+name+' right?') != true){
-    name = prompt('Hey, what is your name?');
-}
-
-alert('Hello '+name);
-
--- Learn_JavaScript/basic/basic_objects.js
+++
@@ -1,7 +0,0 @@
-var n = 20;
-var s = 'foobar';
-var bool = true;
-
-console.log(n);
-console.log(s);
-console.log(bool);
