4cb61cfc8d33d1bc6755edfc257c168b5fb91a3a
Kevin Guan <KevinGuan.gm@gmail.com>
2016-02-07 09:46:26 +0800

    Removed useless programs; added 'only_read_one_letter.py'

--- Python/programs/lower.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import sys
-text = sys.stdin.read()
-print('\n\n\n\n')
-text = text.lower()
-print(text[0].upper()+text[1:])
--- Python/useful/BeautifulSoup_prettify.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-import requests
-from bs4 import BeautifulSoup
-
-r = requests.get(sys.argv[1])
-soup = BeautifulSoup(r.text, "html.parser")
-
-print(soup.prettify())
