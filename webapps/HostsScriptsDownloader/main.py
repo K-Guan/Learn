7029cdd376fada7944efb839047a55d28b46f8bf
Kevin Guan <KevinGuan.gm@gmail.com>
2016-02-20 17:05:04 +0800

    Added HostsScriptsDownloader

---
+++ webapps/HostsScriptsDownloader/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import requests
+from io import BytesIO
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+
+@app.route('/hosts_scripts/script_tool_for_linux.sh')
+def linux_version():
+    link = 'https://raw.githubusercontent.com/racaljk/hosts' +\
+        '/master/hosts_tools/scripts/script_tool_for_linux.sh'
+
+    return send_file(BytesIO(requests.get(link).content))
+
+
+@app.route('/hosts_scripts/script_tool_for_windows.bat')
+def windows_version():
+    link = 'https://raw.githubusercontent.com/racaljk/hosts' +\
+        '/master/hosts_tools/scripts/script_tool_for_windows.bat'
+
+    return send_file(BytesIO(requests.get(link).content))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
