dce69cc3789a0e13917ab9945fdfcee5a4eee702
Kevin Guan <KevinGuan.gm@gmail.com>
2016-04-13 11:06:29 +0800

    Use arrow function everywhere

--- JavaScript/Node/readFileAsync.js
+++ JavaScript/Node/readFileAsync.js
@@ -1,4 +1,4 @@
-var readFileAsync = path => new Promise((resolve, reject) => fs.readFile(path, function(error, data) {
+var readFileAsync = path => new Promise((resolve, reject) => fs.readFile(path, (error, data) => {
     if (error) reject(error);
     resolve(data);
 }));
